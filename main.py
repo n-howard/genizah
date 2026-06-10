dc034a3be6a3bfcdfa763e9c938d98a1fe406594
Nat Howard <nhoward@oberlin.edu>
2026-06-10 19:02:54 +0100

    categories A-K

--- main.py
+++ main.py
@@ -22,45 +22,76 @@ def main():
     rows = []
     for entry in entries:
         dict = {}
-        if ". Parchment" in entry:
-            entry = entry.replace(". Parchment", ".\nParchment")
+       
+        entry = (
+            entry
+                .replace(". Parchment", ".\nParchment")
+                .replace("; Pa", "\nPa")
+                .replace("; pa", "\npa")
+                .replace("mutilated", "damaged")
+                .replace(";\n", "; ")
+                .replace("\n;", ";")
+                
+        )
+        entry = re.sub(r"(?<=;)\n(?=\d)", " ", entry)
+        pattern = rf"({re.escape("The tetragrammaton is abbreviated")})[^.]*"
+        entry = re.sub(pattern, r"\1", entry)
+        pattern2 = r"(damaged.*?\.) "
+        entry = re.sub(pattern2, r"\1\n", entry)
         lines = entry.split("\n")
         lines[:] = [x for x in lines if x]
+        if len(lines)==0:
+            continue
         print(lines)
         if len(lines) < 3:
+            dict['A'] = lines[0]
+            dict['K'] = lines[1] if len(lines)>1 else ''
+            rows.append(dict)
             continue
+
+        if "archment" in lines[2] or "aper" in lines[2]:
+            lines.insert(2, "")
+
         if "tetragrammaton" in entry:
             dict.update({'B': "Bible-Related"})
-            pattern = rf"({re.escape("The tetragrammaton is abbreviated")})[^.]*"
-            lines[4] = re.sub(pattern, r"\1", lines[4])
+            
         else:
             dict.update({'B': "Bible"})
-        if "mutilated" in entry:
-            lines[3] = lines[3].replace("mutilated", "damaged")
+                
         dict.update({'A': lines[0]})
         K = lines[1] + "; " + lines[4] if len(lines)==5 else lines[1]
         dict.update({'K': K, 'J': lines[2]})
-        vals = lines[3].split(";")
-        if "col" not in vals[2]:
-            vals.insert(2, "")
-        if "lea" not in vals[3]:
-            vals.insert(3, "")
-        if "line" not in vals[4]:
-            vals.insert(4, "")
-        if len(vals) < 6:
-            vals.append("")
-        dict.update({'C': vals[0], 'F': vals[3], 
-                    'G': vals[2],'H': vals[4], 'I': vals[5]})
-        dims = vals[1].split("x")
-        if len(dims)==2:
-            dict.update({'D': dims[0],'E':  dims[1]})
-        else:
-            h = dims[::2]
-            w = dims[1::2]
-            delim = "; "
-            height = delim.join(h)
-            width = delim.join(w)
-            dict.update({'D': height,'E': width})
+        if len(lines) >= 4:
+            vals = lines[3].split(";")
+            if len(vals)==1:
+                dict['K'] = K + vals[0]
+                rows.append(dict)
+                continue
+            if len(vals) <5 and len(lines)>4 and ";" in lines[4]:
+                vals2 = lines[4].split(";")
+                vals = vals + vals2
+                lines[3] =  lines[3]+lines[4]
+                lines.remove(lines[4])
+            if len(vals)<3 or "col" not in vals[2]:
+                vals.insert(2, "")
+            if  len(vals)<4 or "lea" not in vals[3] :
+                vals.insert(3, "")
+            if  len(vals)<5 or "line" not in vals[4] :
+                vals.insert(4, "")
+            if len(vals) < 6:
+                vals.append("")
+            dict.update({'C': vals[0], 'F': vals[3], 
+                        'G': vals[2],'H': vals[4], 'I': vals[5]})
+            dims = vals[1].split("x")
+            if len(dims)==2:
+                dict.update({'D': dims[0],'E':  dims[1]})
+            else:
+                h = dims[::2]
+                w = dims[1::2]
+                delim = "; "
+                height = delim.join(h)
+                width = delim.join(w)
+                dict.update({'D': height,'E': width})
         rows.append(dict)
     
     df = pd.DataFrame(rows, 
