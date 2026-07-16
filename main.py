68530787c4287fa51de1bda95e2c4166fa4dfa9c
Nat Howard <nhoward@oberlin.edu>
2026-07-16 17:16:20 +0100

    finished hebrew and solved some coding problems

--- main.py
+++ main.py
@@ -36,6 +36,13 @@ def main():
         bibEntry = bibEntry.replace(" ;", ";")
         label = re.findall(r"(?<=Label: \S)([^\n]*)", bibEntry)
         label = label[0]
+        if "Or" in label:
+            label = re.sub(r"(Or\.)\s*(\d+)(?:\s)([\w\.]+)", r"\1 \2.\3", label)
+            tPattern = r"(T-S\s*\w*\s*\d+\.\d+|Or\.\s*\d+(?:\.\w+)*|Wm.\s*\S*\s*\d+(?:\.\d+)*)"
+            subLabel = re.findall(tPattern, label)
+            label = subLabel[0]
+        if "Misc." in label:
+            label = label.replace("Misc.", "Misc. ")
         bibDict[label] = bibEntry
 
     rows = []
@@ -43,9 +50,9 @@ def main():
     realWords = ["non", "Judaeo", "Kittel", "prayer", "writ", "megillah", "pen", "branch", "Strauss", "right", "line", "one", "prev", "page", "title", 
                      "century","poll", "book", "al-Rida", "Dhual", "folio", "wa", "week", "ha-", "quarter", "drawn", "word", "half", "Tel", "end", "crossed", "pre", "al-", 
                      "Mosin", "wide", "he-", "ad-", "hand", "Judeao", "ink", "double", "space", "standard", "small", "side", "bibl"]
-    hebCount = 0
-    hebList = []
-   
+    # hebCount = 0
+    # hebList = []
+
     for entry in entries:
         
         dict = {}
@@ -64,18 +71,27 @@ def main():
                 .replace(" ,", ",")
                 .replace("®", "")
                 .replace("ר lines", "7 lines")
+                .replace("ר leaves", "7 leaves")
                 .replace("׳,", ";")
                 .replace("ו־", "ר")
-                
+                .replace("0f", "of")
+                .replace("*", "'")
+                .replace("hafiarah", "haftarah")
+                .replace(";;", ";")
+                .replace(",,", ",")
+                .replace(". Hebrew;", ".\nHebrew;")
+                .replace(", On", ". On")
+                .replace(". which", ", which")
         )
         
         entry = re.sub(r"(?<=\.)\s*י+\]", "", entry)
         paPattern = r"([.;])\s*(Parchment|Paper|leather)"
         entry = re.sub(paPattern, r"\1\n\2",entry, flags=re.IGNORECASE)
         entry = re.sub(r"(?<=;)\n(?=\d)", " ", entry)
-        pattern = r"(The tetragrammaton is (?:sometimes\s|once\s)*abbreviated)[^.]*"
-        entry = re.sub(pattern, r"\1", entry, flags=re.IGNORECASE)
-        entry = re.sub(r"(The tetragrammaton is abbreviated.)\s*[\u0590-\u05fe]*\]", r"\1", entry, flags=re.IGNORECASE)
+        # pattern = r"(The tetragrammaton is (?:sometimes\s|once\s)*abbreviated)[^.]*"
+        # entry = re.sub(pattern, r"\1", entry, flags=re.IGNORECASE)
+        # entry = re.sub(r"(The tetragrammaton is abbreviated.)\s*[\u0590-\u05fe]*\]", r"\1", entry, flags=re.IGNORECASE)
+        # entry = re.sub(r"(The tetragrammaton is abbreviated.)\S*", r"\1", entry, flags=re.IGNORECASE)
         pattern2 = r"(damaged.*?\.) "
         entry = re.sub(pattern2, r"\1\n", entry, flags=re.IGNORECASE)
         specialPattern = r"([A-Za-z])[־׳]"
@@ -84,8 +100,19 @@ def main():
         entry = re.sub(r"([\u05d0-\u05ea])\s([\u05d0-\u05ea])\s", r"\1\2", entry)
         entry = re.sub(r"([A-Za-z])([\u0590-\u05fe])", r"\1 \2", entry)
         entry = re.sub(r"([\u0590-\u05fe])([A-Za-z])", r"\1 \2", entry)
+        entry = re.sub(r"(\d)[-—–]ר", r"\1-7", entry)
+        entry = re.sub(r"ר[-—–](\d)", r"7-\1", entry)
+        entry = re.sub(r"(?<=[\s\d])[l](?=[-\s\d—–:vr])", r"1", entry)
+
+        entry = re.sub(r"\n(with|damaged|rubbed|missing|piece|many|of|or|small|few|preserved|repaired|only|and|illegible|a|on|letter|word|text|identified|slightly|\(incomplete\)|side|\)|folio|lines|in|form|follow|fragment|\(|is|abbreviated)", r" \1", entry)
+        entry = re.sub(r"([,;x])\n(\d)", r" \1 \2", entry, flags=re.I)
         
         entry = re.sub(r"([A-Za-z]{2,})-\s*([A-Za-z]{2,})", lambda match: match.group(1)+ "-" +match.group(2) if match.group(1)+ "-" +match.group(2) in spell or any(real.lower() in (match.group(1)+ "-" +match.group(2)).lower() for real in realWords) else match.group(1)+match.group(2), entry, flags=re.IGNORECASE)
+
+        pattern = r"(The tetragrammaton is (?:sometimes\s|once\s)*abbreviated)[^.]*"
+        entry = re.sub(pattern, r"\1", entry, flags=re.IGNORECASE)
+        entry = re.sub(r"(The tetragrammaton is abbreviated.)\s*[י\]\[]{2,}(\sי)*", r"\1", entry, flags=re.IGNORECASE)
+        entry = re.sub(r"(The tetragrammaton is abbreviated.)\S*", r"\1", entry, flags=re.IGNORECASE)
         
         # entry = re.sub(r"(?<=[^\u25a0-\u25ff\s\[\]])[\u25a0-\u25ff](?=[^\u25a0-\u25ff\s\[\]])", "", entry)
         entry = re.sub(r"decorated\s*[\u25a0-\u25ff]", r"decorated ס", entry)
@@ -94,7 +121,11 @@ def main():
         entry = re.sub(r"(\d{1,2})(\d{1,2})\s*[\u0590-\u05fe]\s*lines", r"\1-\2 lines", entry)
 
         entry = re.sub(r"([\u0590-\u05fe])\s*([;,])\s*([\u0590-\u05fe])", r"\1 \2\3", entry)
+
+        entry = re.sub(r"(\d) (Hebrew)", r"\1\n\2", entry)
         
+        entry = re.sub(r"[ ]{2,}", r" ", entry)
+
         lines = entry.split("\n")
         lines[:] = [x for x in lines if x]
 
@@ -103,35 +134,58 @@ def main():
     
         dict['A'] = lines[0].strip().rstrip(';')
 
-        tPattern = r"(T-S\s*\w*\s*\d+\.\d+|Or\.\s*\d+(?:\.\d+)*|Wm.\s*\S*\s*\d+(?:\.\d+)*)"
+        tPattern = r"(T-S\s*\w*\s*\d+\.\d+|Or\.\s*\d+(?:\.\w+)*|Wm.\s*\S*\s*\d+(?:\.\d+)*)"
         titles =  re.findall(tPattern, lines[0])
         if len(titles) == 0:
             titles.append(lines[0])
+            
         dict['L'] = bibDict[titles[0]] if titles[0] in bibDict else ''
         
 
         if re.findall(r"\[\d+\]$", lines[-1]):
             lines.remove(lines[-1])
 
-        if "tetragrammaton is abbreviated" in entry or bool(re.search(r"liturgical poetry", entry, flags=re.IGNORECASE)) or "child" in entry or "children" in entry or bool(re.search(r"writing[\-\s]*exercise", entry)):
+        if "tetragrammaton is abbreviated" in entry or bool(re.search(r"(liturgical|liturgy)", entry, flags=re.IGNORECASE)) or "child" in entry or "children" in entry or bool(re.search(r"writing[\-\s]*exercise", entry)) or "Haggadah" in entry or "Mezuzah" in entry or "tefillin" in entry or "Maimonedes" in entry or "Midrash" in entry or "Mishnah" in entry:
             dict.update({'B': "Bible-Related"})
-            
+
+        elif (bool(re.search(r"unidentified", entry, flags=re.IGNORECASE)) and not bool(re.search(r"Hebrew Bible", entry, flags=re.IGNORECASE))):
+            dict['B'] = "Bible-Related"
+
         else:
             dict.update({'B': "Bible"})
 
+        if (not bool(re.search(r"(lea(ves|f))|line|col|size|cm|(\d\s*x\s*\d)|dama", entry, flags = re.IGNORECASE))) and len(lines)>2:
+            lines[1] = " ".join(lines[1:])
+            lines = lines[:2]
+
+        if len(lines)>=3:
+            if re.search(r"(^and)|(^with)|\d{2,}|(Mezuzah)|(^Targum)|(liturgical)|(Tephillin)|(manuscript)|(fragment)|(Triennial)", lines[2], flags=re.I):
+                lines[1] = " ".join(lines[1:3])
+                lines.remove(lines[2])
+               
+            # if ". Hebrew;" in lines[1] or bool(re.search(r"\d Hebrew;", lines[1])):
+            if " Hebrew;" in lines[1]:
+                lines[1] = re.sub(r" (Hebrew;)", r"\n\1", lines[1])
+                newLines = lines[1].split("\n")
+                lines[1]=newLines[0]
+                lines.insert(2, newLines[1])
+               
+
         if len(lines) < 3:
-            if bool(re.search(r"[\u0590-\u05fe]", entry)):
-                hebCount+=1
-                hebList.append(lines[0]) 
+            # if bool(re.search(r"[\u0590-\u05fe]", entry)):
+            #     hebCount+=1
+            #     hebList.append(lines[0]) 
             if "archment" and "aper" in entry:
-                dict['C'] = "parchment; paper"
+                dict['C'] = "Parchment; Paper"
             elif "archment" in entry:
-                dict['C'] = "parchment"
-            elif "paper" in entry:
-                dict['C'] = "paper"
-            elif "leather" in entry:
-                dict['C'] = "leather"
-            dict['K'] = lines[1] if len(lines)>1 else ''
+                dict['C'] = "Parchment"
+            elif "aper" in entry:
+                dict['C'] = "Paper"
+            elif "eather" in entry:
+                dict['C'] = "Leather"
+            if len(lines) > 1:
+                lines[1] = re.sub(r";\s*(parchment|paper|leather)\.*$", r"", lines[1], flags=re.I)
+                dict['K'] = lines[1]
             if "See" in entry and "Klein" not in entry:
                 problems.append(",".join(lines))
                 continue
@@ -140,10 +194,13 @@ def main():
             rows.append(dict)
             continue
 
+
         if "archment" in lines[2] or "aper" in lines[2] or "eather" in lines[2]:
             lines.insert(2, "")
         
+        
 
+        
         
         # if len(lines)>=4:
         #     if "archment" not in lines[3] and "aper" not in lines[3]:
@@ -155,8 +212,8 @@ def main():
             lines[3] = re.sub(r"(?<=\d)־", "-", lines[3])
             vals = lines[3].split(";")
             if len(vals)<=1:
-                if "aper" or "archment" in lines[3]:
-                    dict['C'] = lines[3].strip().rstrip(';')
+                if "aper" or "archment" or "eather" in lines[3]:
+                    dict['C'] = lines[3].strip().rstrip(';').title()
                     dict['K'] = lines[1].strip().rstrip(';')
                     rows.append(dict)
                     continue
@@ -185,7 +242,7 @@ def main():
                     continue
                 elif "col" in val:
                     G.append(val.strip())
-                elif "lea" in val:
+                elif "leaf" in val or "leaves" in val:
                     F.append(val.strip())
                 elif "line" in val:
                     H.append(val.strip())
@@ -194,13 +251,13 @@ def main():
             G = delim.join(G).rstrip(';').replace(".", "").strip()
             F = delim.join(F).rstrip(';').replace(".", "").strip()
             H = delim.join(H).rstrip(';').replace(".", "").strip()
-            C = delim.join(C).rstrip(';').replace(".", "").strip()
+            C = delim.join(C).rstrip(';').replace(".", "").strip().title()
             dict.update({'C': C, 'F': F, 
-                        'G': G,'H': H, 'I': vals[-1].strip().rstrip(';')})
+                        'G': G,'H': H, 'I': vals[-1].strip().rstrip(';:.')})
             if "average" in vals[1] or "size" in vals[1]:
                 vals[1] = re.sub(r"([0-9a-zA-Z\.\s]*)\sx\s([0-9a-zA-Z\.\s]*)\s[=\()]+\s*([0-9a-zA-Z\.\s]*)[\)]*", r"\1 = \3 x \2 = \3", vals[1])
             vals[1] = re.sub(r"(\d)\.\s[l]", r"\1.1", vals[1])
-            dims = re.split(r"and|x|,", vals[1])
+            dims = re.split(r"and|x|,", vals[1], flags=re.IGNORECASE)
             dims = [s.strip() for s in dims]
             dims[:] = [x for x in dims if x]
             if len(dims)==2:
@@ -241,8 +298,8 @@ def main():
         vocPattern = r"(vocalized with the non-standard)[^.]*(\.)?"
         K = re.sub(vocPattern, r"\1 form.", K)
 
-        signsPattern = r"(?:This|the)\s*(sign|line ending)([s])?.*?(\.|,|\s?are|\s?is|\s?marks|$)"
-        K = re.sub(signsPattern, lambda match: match.group(1)+"s"+match.group(3) if (match.group(3)==" are" or match.group(2)=="s") else "a "+match.group(1)+match.group(3), K, flags=re.IGNORECASE)
+        signsPattern = r"(?:This|the)\s*(sign|line ending)([s])?.*?(\.|,|are|is|marks|$)"
+        K = re.sub(signsPattern, lambda match: match.group(1)+"s "+match.group(3) if (match.group(3)=="are" or match.group(2)=="s") else "a "+match.group(1)+" "+match.group(3), K, flags=re.IGNORECASE)
         K = re.sub(r"[;\.]\s+[a-z](?=[^\.]{3,})", lambda match: match.group(0).upper(), K)
 
         K = re.sub(r"\[\s*1\s*(?=[^\]0-9])|(?<=[^\[0-9])\s*1\s*\]", "[ ]", K)
@@ -252,6 +309,10 @@ def main():
         
         K = re.sub(onePattern, r"\1[\3", K)
         K = re.sub(onePattern2, r"\1]\3", K)
+
+        continuedPattern = r"\[\s*" + re.escape(titles[0]) + r"[,\s]*continued\s*\]"
+
+        K = re.sub(continuedPattern, r"", K, flags=re.IGNORECASE)
 
         K = re.sub(r"(?<=\d)־", ",", K)
         
@@ -280,18 +341,20 @@ def main():
         K = re.sub(r" n([\s,\.])", r" ח\1", K)
         K = re.sub(r"\s([\u0590-\u05fe])\s(s)([\s,\.])", r" \1\2\3", K)
 
-        K = K.replace("יטראל", "ישראל")
+        K = K.replace("יטראל", "ישראל").replace(";;", ";").replace(",,", ",")
 
-        # HEBCOUNT START
-        if bool(re.search(r"[\u0590-\u05fe]", K)):
-            hebCount+=1
-            hebList.append(lines[0]) 
-        # HEBCOUNT END
+        K = re.sub(r"[ ]{2,}", r" ", K)
 
-        if 'I' in dict:
-            if bool(re.search(r"[\u0590-\u05fe]", dict['I'])):
-                hebCount+=1
-                hebList.append(lines[0]) 
+        # # HEBCOUNT START
+        # if bool(re.search(r"[\u0590-\u05fe]", K)):
+        #     hebCount+=1
+        #     hebList.append(lines[0]) 
+        # # HEBCOUNT END
+
+        # if 'I' in dict:
+        #     if bool(re.search(r"[\u0590-\u05fe]", dict['I'])):
+        #         hebCount+=1
+        #         hebList.append(lines[0]) 
         
         # bracketPattern = r"([A-Za-z]{3,})\s*\[\s?\](\s*[A-Za-z.])"
         
@@ -305,32 +368,40 @@ def main():
 
      
 
-        titlePattern = r"((?:T-S\s*[\w\.]*\s*\d+\.\d+|Or\.\s*\d+(?:\.\d+)*|Wm.\s*\S*\s*\d+(?:\.\d+)*)(?:[a-z])?(?:\sand\s\d+)*)"
-        refs = re.findall(titlePattern, K)
-        refs = list(set(refs))
+        titlePattern = r"((?:T-S\s*[\w\.]*\s*\d+\.\d+|Or\.\s*\d+(?:\.\w+)*|Wm.\s*\S*\s*\d+(?:\.\d+)*)(?:[a-z])?(?:\sand\s\d+)*)"
+        refs = re.findall(titlePattern, entry)
         if len(titles)>1:
             refs = refs + titles[1:]
+        if titles[0] in refs:
+            refs.remove(titles[0])
+        refs = list(set(refs))
         refString = "; ".join(refs).strip()
 
+        if not bool(re.search(r"\.$", K)):
+            K = K + "."
+
+        
 
         dict.update({'M': refString, 'K': K.strip().rstrip(';'), 'J': lines[2].rstrip(';').replace(".","").strip()})
+
+
+
         rows.append(dict)
         
     df = pd.DataFrame(rows, 
         columns=['A','B','C','D','E','F','G','H','I','J','K','L','M'])
     df.to_csv("test.csv", index=False)
     
-    curr = "T-S AS 64.50"
-    last = "T-S AS 64.50"
-    completeSoFar = hebList.index(curr)+1
-    percent = (completeSoFar/len(hebList))*100
-    # print(hebList, "\n", hebCount, "\n",completeSoFar , "\n", str(percent)+"%" )
-    print(str(hebCount) + "\n" + str(completeSoFar) + "\n" + str(percent)+"%" )
-    diff = completeSoFar - (hebList.index(last)+1)
-    print(diff)
-
+    # curr = "Or.1080.9.4"
+    # last = "T-S AS 64.50"
+    # completeSoFar = hebList.index(curr)+1
+    # percent = (completeSoFar/len(hebList))*100
+    # # print(hebList, "\n", hebCount, "\n",completeSoFar , "\n", str(percent)+"%" )
+    # print(str(hebCount) + "\n" + str(completeSoFar) + "\n" + str(percent)+"%" )
+    # diff = completeSoFar - (hebList.index(last)+1)
+    # print(diff)
 
     # print(len(problems), problems)
 
-if __name__ == '__main__':
+if __name__ == '__main__': 
     main()
