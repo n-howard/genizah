45b2641398c7d648da9b7877d7f93a9ce849057e
Nat Howard <nhoward@oberlin.edu>
2026-08-04 12:21:09 +0100

    config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  basePath: "genizah",
-  output: "export",
-  reactStrictMode: true,
-};
+
 
 module.exports = {
   typescript: {
-    ignoreBuildErrors: true,
-    tsconfigPath: 'tsconfig.json',
+    ignoreBuildErrors: true
   },
 }
